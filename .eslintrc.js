d9f29dd4b85b9b3d4ab806090febf59bc3ec65b9
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2024-12-12 13:59:19 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-  "extends": ["eslint:recommended", "plugin:security/recommended"], // Use eslint-plugin-security
-  "env": {
-    "node": true,
-    "es6": true
-  },
-  "rules": {
-    "no-eval": "error",
-    "no-new-wrappers": "error",
-    "no-template-curly-in-string": "error",
-    "no-unsafe-regex": "error",
-    "security/detect-non-literal-regexp": "error", // Example of a security-related rule
-    // Add more security rules as required
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        // Add or override rules here
+    },
 };
